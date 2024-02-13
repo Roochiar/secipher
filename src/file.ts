48fef663a066c973488704b902018528fa3421a5
Roochiar <roochiarfa@gmail.com>
2024-02-13 02:14:09 -0800

    finish generate file

--- src/file.ts
+++ src/file.ts
@@ -1,6 +1,7 @@
 import fs from "fs"
 import os from 'os'
 import { createKey } from "./createKey.js"
+import type { AllFile, KeyFile } from './types.js'
 
 class File {
     private file: {
@@ -15,29 +16,51 @@ class File {
 
     private fakeFiles: string[] = []
 
-    constructor(url: fs.PathOrFileDescriptor, name?: string) {
+    public async request(url: fs.PathOrFileDescriptor, options?: { name?: string, password?: string }) {
         if (this.file.name && this.file.dir) {
 
-            this.rename(url)
+            await this.rename(url)
         } else {
-            if (name) {
+            if (options?.name) {
+                const prev = this.file
                 this.file = {
                     ...this.file,
-                    name: name,
+                    ...options,
                     dir: url
                 }
+                await this.read()
+                    .then(res => {
+                        return { status: true, res }
+                    })
+                    .catch(err => {
+                        this.file = prev
+                        return { status: false, err }
+                    })
+            } else {
+                await this.create(url)
+                    .then(res => {
+                        return { status: true, res }
+                    })
+                    .catch(err => {
+                        return { status: false, err }
+                    })
             }
-            console.log(this.file);
-            this.create(url)
         }
     }
 
-    private read() {
+    public setData() {
+
+    }
+
+    public getData() {
+
+    }
+
+    private async read() {
         try {
             fs.readFile(`${this.file.dir + this.file.name}.json`, { encoding: "utf-8" }, (err, data) => {
                 if (err) throw Error(err.message)
 
-                console.log(data);
                 return data
             })
         } catch (err) {
@@ -45,7 +68,7 @@ class File {
         }
     }
 
-    private create(dir: fs.PathOrFileDescriptor) {
+    private async create(dir: fs.PathOrFileDescriptor) {
         const name = createKey(1, "code2").str
         const password = createKey(1, "code2").str
 
@@ -58,7 +81,6 @@ class File {
                     password: password,
                     dir: dir
                 }
-                console.log(this.file);
                 return this.file
             })
         } catch (err) {
@@ -66,11 +88,16 @@ class File {
         }
     }
 
-    private remove() {
+    private async remove() {
         try {
             fs.rm(`${this.file.dir + this.file.name}.json`, err => {
                 if (err) throw Error(err.message)
 
+                this.file = {
+                    name: "",
+                    password: "",
+                    dir: ""
+                }
                 return true
             })
         } catch (err) {
@@ -78,7 +105,7 @@ class File {
         }
     }
 
-    private rename(dir: fs.PathOrFileDescriptor) {
+    private async rename(dir: fs.PathOrFileDescriptor) {
         const name = createKey(1, "code2").str
         const password = createKey(1, "code2").str
 
@@ -97,8 +124,42 @@ class File {
             return err
         }
     }
+
+    private async checkData(name: string) {
+        try {
+            let check = false
+
+            await this.read()
+                .then(data => JSON.parse(`${data}`)[name] ? check = true : new Error(`${name} is not found!`))
+                .catch(err => new Error(err))
+
+            return check
+        } catch (err) {
+            return err
+        }
+    }
+
+    private async newData(name: string, value: KeyFile) {
+        try {
+            await this.checkData(name)
+                .then(() => new Error("This name exists"))
+
+
+            await this.read()
+                .then(data => add(data))
+                .catch(err => new Error(err))
+
+            const add = (obj: unknown) => {
+                let prevData: AllFile = JSON.parse(`${obj}`)
+
+                prevData[name] = value
+            }
+        } catch (err) {
+            return err
+        }
+    }
 }
 
-export default function getFile(url: fs.PathOrFileDescriptor) {
-    new File(url)
+export default async function getFile(url: fs.PathOrFileDescriptor, options?: { name?: string, password?: string }) {
+    return await new File().request(url)
 }
